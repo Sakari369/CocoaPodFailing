abe55235e3d0fc3d513338717767d3013c6eb8c5
Sakari Lehtonen <sakari@psitriangle.net>
2023-02-17 01:09:21 +0100

    Move files

--- vendored_lib/vendored_lib.c
+++
@@ -1,7 +0,0 @@
-#include "vendored_lib.h"
-
-FFI_PLUGIN_EXPORT intptr_t vendored_function(intptr_t a) {
-  printf("vendored_function been called mon! a = %ld", a);
-
-  return a * 32;
-}
--- vendored_lib/vendored_lib.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-
-#define FFI_PLUGIN_EXPORT
-
-FFI_PLUGIN_EXPORT intptr_t vendored_function(intptr_t a);
